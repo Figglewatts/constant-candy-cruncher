18fcf0cd4688edce765972dddf339047e586a263
Figglewatts <figglewatts@hotmail.co.uk>
2014-01-28 15:49:46 +0000

    Added fixed timestep and framerate to counter jittering

--- constant-candy-cruncher/src/figglewatts/constantcandycruncher/ConstantCandyCruncher.java
+++ constant-candy-cruncher/src/figglewatts/constantcandycruncher/ConstantCandyCruncher.java
@@ -15,29 +15,53 @@ import figglewatts.constantcandycruncher.screen.PlayGameScreen;
 
 public class ConstantCandyCruncher implements ApplicationListener {
 	
-	PlayGameScreen test = new PlayGameScreen();
+	private final float TIMESTEP = 1f / 60f;
+	private final float FRAMERATE = 1f / 120f;
+	private double updateAccumulator = 0d;
+	private double renderAccumulator = 0d;
+	private long oldTime = System.nanoTime();
+	private long currentTime = System.nanoTime();
+	private double delta = 0.0f;
+	
+	private PlayGameScreen playGameScreen = new PlayGameScreen();
 	
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		test.show();
+		playGameScreen.show();
 	}
 
 	@Override
 	public void dispose() {
-		test.dispose();
+		playGameScreen.dispose();
 	}
 
 	@Override
 	public void render() {		
-		test.render(Gdx.graphics.getDeltaTime());
+		oldTime = currentTime;
+		currentTime = System.nanoTime();
+		
+		// provides delta in seconds with nanosecond accuracy
+		// e.g. 0.003 is 0 seconds and 3 milliseconds (or 3x10^-3 seconds)
+		delta = (double)(currentTime - oldTime) * 0.000000001f;
+		
+		updateAccumulator += delta;
+		while(updateAccumulator > TIMESTEP) {
+			// update
+			playGameScreen.update(delta);
+			updateAccumulator -= TIMESTEP;
+		}
+		
+		renderAccumulator += delta;
+		if (renderAccumulator > FRAMERATE) {
+			// draw
+			playGameScreen.render(Gdx.graphics.getDeltaTime());
+			renderAccumulator = 0.0f;
+		}
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		test.resize(width, height);
+		playGameScreen.resize(width, height);
 	}
 
 	@Override
